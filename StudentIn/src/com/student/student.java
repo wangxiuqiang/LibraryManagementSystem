81bed1cf54fd428515a310cfd2c6a9df5261b2bf
wxq <wangxiuqianga@163.com>
2017-05-23 10:38:26 +0800

    图书馆管理系统的学生注册

---
+++ StudentIn/src/com/student/student.java
@@ -0,0 +1,14 @@
+package com.student;
+
+public class student {
+        public String name;
+        public int id;
+        public int money;
+        public int bookcount;
+        public student(int id,String name,int money){
+        	this.id=id;
+        	this.name=name;
+        	this.money=money;
+        }
+		
+}
